6e8f086e5b4b0665ab3836a32e290fee25a1d8d1
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-01-18 17:16:21 +0100

    Added invoices controller and copied code from AdminLTE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,6 +28,7 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
     //
+    Route::get('/invoices', 'InvoicesController@index');
 });
 
 Route::group(['middleware' => 'web'], function () {
@@ -36,8 +37,3 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/home', 'HomeController@index');
 });
 
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
-
-    Route::get('/home', 'HomeController@index');
-});
